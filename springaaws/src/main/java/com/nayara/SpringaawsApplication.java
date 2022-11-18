ae7fe18182557eeedbc782b561604da7e0253ea6
Nayara Mudesto <[200~git config --global user.email [200>
2022-11-17 22:33:15 -0300

    add greeting endpoint

---
+++ springaaws/src/main/java/com/nayara/SpringaawsApplication.java
@@ -0,0 +1,13 @@
+package com.nayara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaawsApplication.class, args);
+	}
+
+}
---
+++ springaaws/src/test/java/com/springaaws/SpringaawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
